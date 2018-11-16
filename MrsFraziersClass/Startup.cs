0203bbde20f6b941ea25fa0aa4b7776795e48697
Eli-Frazier <42358273+Eli-Frazier@users.noreply.github.com>
2018-11-16 14:53:38 -0500

    Initial commit

---
+++ MrsFraziersClass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MrsFraziersClass.Startup))]
+namespace MrsFraziersClass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
